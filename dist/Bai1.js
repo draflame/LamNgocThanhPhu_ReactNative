5340f0a0b712761e2ce78d4fcd1e69e9bc41cf7f
H82M19\Student <Student>
2025-08-20 07:52:50 +0700

    Bai 1: Create a class Person with attributes name and age. Write a method to display this information.

---
+++ dist/Bai1.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    ;
+}
+const person = new Person("Lam Ngoc Thanh Phu", 21);
+function display(person) {
+    console.log(person);
+}
+display(person);
+//# sourceMappingURL=Bai1.js.map
---
+++ src/Bai1.ts
@@ -0,0 +1,13 @@
+class Person{
+    constructor(name:string, age:number){
+        this.name=name;
+        this.age=age
+    };
+    name: string;
+    age:number
+}
+const person:Person=new Person("Lam Ngoc Thanh Phu",21);
+function display(person: Person):void{
+    console.log(person)
+}
+display(person)
