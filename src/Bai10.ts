cd7462f728480a035730c2351a6cd9b6cb167e4b
draflame <phulam03@gmail.com>
2025-08-24 01:06:26 +0700

    bai 10: 10. Create a class Account with public, private and readonly fields.

---
+++ src/Bai10.ts
@@ -0,0 +1,11 @@
+class Account {
+  public accountNumber: string;
+  private balance: number;
+  readonly createdAt: Date;
+
+  constructor(accountNumber: string, initialBalance: number) {
+    this.accountNumber = accountNumber;
+    this.balance = initialBalance;
+    this.createdAt = new Date();
+  }
+}
