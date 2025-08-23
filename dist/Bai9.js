4e3f1fd0713a1b04e6b0a8dcea3b0dbd28266dd4
draflame <phulam03@gmail.com>
2025-08-24 01:03:28 +0700

    Bai 9: Define an interface Animal with name and method sound().

---
+++ dist/Bai9.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Dog {
+    constructor(name) {
+        this.name = name;
+    }
+    sound() {
+        return "Woof Woof";
+    }
+}
+const dog = new Dog("Dog");
+console.log(`${dog.name} sound: ${dog.sound()}`);
+//# sourceMappingURL=Bai9.js.map
