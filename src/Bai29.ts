0c947dd4f61cc115d66d1913fc7169969299f766
Lâm Ngọc Thanh Phú <phulam03@gmail.com>
2025-08-24 16:57:32 +0700

    Bai 29: Create an interface Movable with method move(). Implement it in Car and Robot.

---
+++ src/Bai29.ts
@@ -0,0 +1,18 @@
+interface Movable {
+  move(): void;
+}
+class Car implements Movable {
+  move(): void {
+    console.log("The car is driving on the road.");
+  }
+}
+class Robot implements Movable {
+  move(): void {
+    console.log("The robot is walking forward.");
+  }
+}
+const myCar: Movable = new Car();
+myCar.move(); 
+
+const myRobot: Movable = new Robot();
+myRobot.move();
