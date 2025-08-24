89b4c221735b5e479e3979217b074ab691d38800
Lâm Ngọc Thanh Phú <phulam03@gmail.com>
2025-08-24 15:20:44 +0700

    Bai 25: 25. Create a class Shape with a static method describe().

---
+++ dist/Bai25.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Shape {
+    static describe() {
+        console.log("This is a shape.");
+    }
+}
+console.log(Shape.describe());
+//# sourceMappingURL=Bai25.js.map
---
+++ src/Bai25.ts
@@ -0,0 +1,6 @@
+class Shape{
+    static describe(): void {
+        console.log("This is a shape.");
+    }
+}
+console.log(Shape.describe());
