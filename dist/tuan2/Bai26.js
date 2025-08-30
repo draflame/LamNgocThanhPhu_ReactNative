473c8b570dffc0f65e1107a29b429bfb7e1040a2
Lâm Ngọc Thanh Phú <phulam03@gmail.com>
2025-08-30 21:43:51 +0700

    Tuan 2: 26. Use async/await with setTimeout to simulate a 5-second wait.

---
+++ dist/tuan2/Bai26.js
@@ -0,0 +1,23 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+function wait(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+function simulateWait() {
+    return __awaiter(this, void 0, void 0, function* () {
+        console.log("Waiting 5 seconds...");
+        yield wait(5000);
+        console.log("Done waiting!");
+    });
+}
+simulateWait();
+//# sourceMappingURL=Bai26.js.map
